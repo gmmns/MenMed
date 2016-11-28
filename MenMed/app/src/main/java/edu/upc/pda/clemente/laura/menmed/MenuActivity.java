8943056ec7516cffff6c1f44715459ac73341e23
lauraclemente <clementemartilaura@gmail.com>
2016-11-28 15:32:11 +0100

    Projecte Android inicial

---
+++ MenMed/app/src/main/java/edu/upc/pda/clemente/laura/menmed/MenuActivity.java
@@ -0,0 +1,12 @@
+package edu.upc.pda.clemente.laura.menmed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}

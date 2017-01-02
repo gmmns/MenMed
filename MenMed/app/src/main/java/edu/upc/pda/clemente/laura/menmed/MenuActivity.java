b4a6ab1cc49bf087efa8070a502d33a913823a76
gmmns <gemma.ns.16@gmail.com>
2017-01-02 13:58:48 +0100

    Canvi d'activitat. Funcionava, de sobte ho ha deixat de fer... no ho entenc. En un altre moment segueixo mirant.

--- MenMed/app/src/main/java/edu/upc/pda/clemente/laura/menmed/MenuActivity.java
+++ MenMed/app/src/main/java/edu/upc/pda/clemente/laura/menmed/MenuActivity.java
@@ -2,6 +2,7 @@ package edu.upc.pda.clemente.laura.menmed;
 
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -27,7 +28,7 @@ public class MenuActivity extends AppCompatActivity{
             R.id.esm_recept, R.id.mig_recept, R.id.dinar_recept1, R.id.dinar_recept2, R.id.dinar_recept3, R.id.ber_recept, R.id.sopar_recept1, R.id.sopar_recept2, R.id.sopar_recept3
     };
     private Menu menu;
-    private Dia dia;
+    private ImageButton btn_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +37,7 @@ public class MenuActivity extends AppCompatActivity{
         all_recipes = getResources().getStringArray(R.array.all_recipes);
         addMenu();
         showDay(menu.getMenu()[1]);
-
+        init();
     }
 
     private void addMenu(){
@@ -48,6 +49,17 @@ public class MenuActivity extends AppCompatActivity{
             TextView recept_text = (TextView) findViewById(ids_recipes[i]);
             recept_text.setText(dia.getS_dia()[i]);
         }
+    }
+
+    protected void init(){
+        btn_list = (ImageButton) findViewById(R.id.btn_list);
+        btn_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MenuActivity.this, ListActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
 
